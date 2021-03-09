713b4d30ac71895b910d806afcc932ced765f37c
DebuggingMyLife <oatkittivises@gmail.com>
2021-03-09 15:56:30 +0700

    Reminder.py Bug fixed

--- Cogs/Reminder.py
+++ Cogs/Reminder.py
@@ -6,12 +6,11 @@ import datetime
 class Reminder(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
     @commands.command()
     async def RemindMe(self, ctx, time: int = 5, *,message: str ):
-        remindertime = await ctx.send(f"I will remind you in {time} seconds: {message}")
         await ctx.message.delete()
-        await asyncio.sleep(5)
-        await ctx.remindertime.delete()
+        remindertime = await ctx.send(f"I will remind you in {time} seconds: {message}")
         await asyncio.sleep(time)
         await ctx.send(f"Reminder for {ctx.author.mention} : {message}")
 def setup(bot):

9d89b3d4e75c8694809c78fb9dbc3d7e46a1d03f
Clockwork00 <heichitc@gmail.com>
2021-04-04 12:08:35 +0800

    something that will never happen to me

--- Cogs/Weebs.py
+++ Cogs/Weebs.py
@@ -118,5 +118,36 @@ class Anime(commands.Cog):
             )
         await ctx.send(embed=embed)
 
+    @commands.command()
+    @commands.bot_has_permissions(embed_links=True)
+    async def kiss(self, ctx: commands.Context):
+        """Generates random gif of a random anime kissing scene"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get("https://api.willz.repl.co/anime/kiss?key=R7SDV-EfV2V-7FWZC-UIUvQ") as response:
+                if response.status == 503:
+                    await ctx.send("The API is actually in maintenance, please retry later.")
+                    return
+                try:
+                    status = response.status
+                    url = await response.json()
+                except aiohttp.ContentTypeError:
+                    await ctx.send(
+                        "API unavailable. Status code: {code}\nIt may be due of a "
+                        "maintenance.".format(code=status)
+                    )
+                    return
+        embed = discord.Embed(
+            title="Here's a gif of a random anime kissing scene!", color=discord.Color.blue()
+        )
+        try:
+            embed.set_image(url=url["url"])
+        except KeyError:
+            await ctx.send(
+                "I received an incorrect format from the API\nStatus code: {code}".format(
+                    code=status
+                )
+            )
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Anime(bot))
